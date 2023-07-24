b0f4b9d867f2dc4e73f834fbfcff6fe137fc84f8
yuvarajyavar <yuvraj.m@yavar.in>
2023-07-24 18:48:31 +0530

    forth commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+/*import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -21,5 +21,75 @@ function App() {
     </div>
   );
 }
+
+export default App;
+*/
+import React, { useState } from 'react';
+
+const App = () => {
+	  const [tasks, setTasks] = useState([
+		      { id: 1, title: 'Task 1' },
+		      { id: 2, title: 'Task 2' },
+		    ]);
+
+	  const [newTaskTitle, setNewTaskTitle] = useState('');
+
+	  const handleInputChange = (event) => {
+		      setNewTaskTitle(event.target.value);
+		    };
+
+	  const handleAddTask = () => {
+		      if (newTaskTitle.trim() !== '') {
+			            const newTask = {
+					            id: tasks.length + 1,
+					            title: newTaskTitle,
+					          };
+
+			            setTasks([...tasks, newTask]);
+			            setNewTaskTitle('');
+			          }
+		    };
+
+	  const handleUpdateTask = (taskId, newTitle) => {
+		      const updatedTasks = tasks.map((task) =>
+			            task.id === taskId ? { ...task, title: newTitle } : task
+			          );
+
+		      setTasks(updatedTasks);
+		    };
+
+	  const handleDeleteTask = (taskId) => {
+		      const filteredTasks = tasks.filter((task) => task.id !== taskId);
+
+		      setTasks(filteredTasks);
+		    };
+
+	  return (
+		      <div>
+		        <h1>Task Manager</h1>
+		        <div>
+		          <input
+		            type="text"
+		            value={newTaskTitle}
+		            onChange={handleInputChange}
+		            placeholder="Enter task title"
+		          />
+		          <button onClick={handleAddTask}>Add Task</button>
+		        </div>
+		        <ul>
+		          {tasks.map((task) => (
+				            <li key={task.id}>
+				              <input
+				                type="text"
+				                value={task.title}
+				                onChange={(e) => handleUpdateTask(task.id, e.target.value)}
+				              />
+				              <button onClick={() => handleDeleteTask(task.id)}>Delete</button>
+				            </li>
+				          ))}
+		        </ul>
+		      </div>
+		    );
+};
 
 export default App;
